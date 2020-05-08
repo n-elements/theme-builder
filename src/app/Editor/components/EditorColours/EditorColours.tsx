d2727a39de2318ebc730d7e19f19b3d518afc4b8
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-08 15:56:46 +0200

    chore: bind variables to editor

--- src/app/Editor/components/EditorColours/EditorColours.tsx
+++ src/app/Editor/components/EditorColours/EditorColours.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import routes from "@routes";
+import { useLocation } from "react-router-dom";
+import useVariables from "../../hooks/useVariables";
 import { EditorWrapper } from "../EditorWrapper";
 import { EditorHeader } from "../EditorHeader";
 import { ColorSwatch } from "../ColorSwatch";
 import classes from "./EditorColours.module.css";
 
 export default function EditorColours() {
+  const location = useLocation();
+  const variables = useVariables(location.pathname);
+
   return (
     <Switch>
       <Route path={routes.editor.colours}>
@@ -16,8 +21,15 @@ export default function EditorColours() {
             subtitle="List of all the available colors"
           />
           <div className={classes.ColorsList}>
-            <ColorSwatch propName="--ne-accent-color" value="#00E5BF" />
-            <ColorSwatch propName="--ne-custom-color-one" value="#974CFF" />
+            {variables.map((variable, index) => (
+              <ColorSwatch
+                key={index}
+                propName={variable.name}
+                value={variable.value}
+              />
+            ))}
+            {/* <ColorSwatch propName="--ne-accent-color" value="#00E5BF" />
+            <ColorSwatch propName="--ne-custom-color-one" value="#974CFF" /> */}
           </div>
         </EditorWrapper>
       </Route>
