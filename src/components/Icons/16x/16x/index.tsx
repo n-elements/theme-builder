37452e5bb5b390715d0f4060e285264ff1ef4159
Mattia Astorino <astorino.mattia@gmail.com>
2020-05-18 18:37:12 +0200

    chore: ignore generated icons components

--- src/components/Icons/16x/16x/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as Auto } from "./Auto";
--- src/components/Icons/24x/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Colors } from "./Colors";
-export { default as Document } from "./Document";
-export { default as Elements } from "./Elements";
-export { default as Typography } from "./Typography";
