41ae3e77d2d53d3abb7e1078ea41b92dd8e3b1d0
octod <iamoctod@gmail.com>
2020-05-25 10:01:37 +0200

    feat: binds theme name to store

--- src/app/Editor/components/ThemeName/ThemeName.tsx
+++ src/app/Editor/components/ThemeName/ThemeName.tsx
@@ -2,20 +2,34 @@ import React from "react";
 import classes from "./ThemeName.module.css";
 import { Edit } from "@components/Icons/12x";
 import clsx from "clsx";
+import { ApplicationState } from "@state";
+import { useSelector, useDispatch } from "react-redux";
+import actions from "@store/actions";
 
-export interface IThemeNameProps extends PropsClass {
-  value?: string;
+export interface IThemeNameProps extends PropsClass {}
+
+function selector(state: ApplicationState) {
+  return state.theming.name;
 }
 
 export const ThemeName = function ({
-  value,
   className,
   ...attributes
 }: IThemeNameProps) {
+  const dispatch = useDispatch();
+  const themeName = useSelector(selector);
+
   return (
     <div className={clsx(classes.ThemeName, className)} {...attributes}>
       <Edit className={classes.Icon} />
-      <input type="text" className={classes.Input} defaultValue={value} />
+      <input
+        className={classes.Input}
+        onChange={(event) =>
+          dispatch(actions.theming.updateName(event.target.value))
+        }
+        type="text"
+        value={themeName}
+      />
     </div>
   );
 };
