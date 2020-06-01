e537ce4d18879a2e67ad2381edba24afe45278a9
octod <iamoctod@gmail.com>
2020-06-01 10:57:38 +0200

    chore: drafts about domain, just in case

---
+++ src/app/About/index.tsx
@@ -0,0 +1,6 @@
+import { Switch } from "react-router-dom";
+import React from "react";
+
+export default function About() {
+  return <Switch></Switch>;
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
+import * as about from "./app/About/routes";
 import * as editor from "./app/Editor/routes";
 
 const root = "/";
 
 export default {
+  about,
   root,
   editor,
 };
