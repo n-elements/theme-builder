a2ae457dbbe7afe410e3d1a57629cdedc058d93f
octod <iamoctod@gmail.com>
2020-06-01 10:45:28 +0200

    feat: hello darkness my old friend, I come to expand hello again dadadaad

--- src/app/Editor/components/ElementsVariables/ElementsVariables.tsx
+++ src/app/Editor/components/ElementsVariables/ElementsVariables.tsx
@@ -1,23 +1,31 @@
 import { Expander } from "@components/Expander";
 import React from "react";
-import { useHistory, useRouteMatch } from "react-router-dom";
+import { useHistory, useRouteMatch, useLocation } from "react-router-dom";
 import useElementsVariables from "@app/Editor/hooks/useElementsVariables";
 import { Variable } from "../Variable";
 import classes from "./ElementsVariables.module.css";
 
 export function ElementsVariables() {
+  type ElementsVariablesState = {
+    open: boolean;
+  };
+
   const elementsvars = useElementsVariables();
-  const history = useHistory();
+  const history = useHistory<ElementsVariablesState>();
   const match = useRouteMatch<{ element: string }>();
+  const location = useLocation<ElementsVariablesState>();
+  const open = location.state?.open ?? true;
 
   return (
     <div ne-details-group="true">
       {elementsvars.elements.map((name, index) => (
         <Expander
-          open={match.params.element === name}
+          open={open ? match.params.element === name : open}
           key={index}
           onExpand={() => {
-            history.push(elementsvars.getRouteForElement(name!));
+            history.push(elementsvars.getRouteForElement(name!), {
+              open: match.params.element === name ? !open : true,
+            });
           }}
           summary={name}
         >
