606a13b5c363e834b68d733bd51e6deb150edd25
octod <iamoctod@gmail.com>
2020-07-02 11:26:31 +0200

    feat: drafts border field

--- src/app/Editor/components/BorderField/BorderField.tsx
+++ src/app/Editor/components/BorderField/BorderField.tsx
@@ -4,10 +4,15 @@ import { TextField } from "../TextField";
 import { ColorField } from "../ColorField";
 import clsx from "clsx";
 import classes from "./BorderField.module.css";
+import useCSSBorder from "@app/Editor/hooks/useCSSBorder";
+import { UnitField } from "../UnitField";
 
 export interface IBorderFieldProps extends IFieldProps {}
 
 export const BorderField = function (props: IBorderFieldProps) {
+  const border = useCSSBorder(props.variable);
+  const variables = border.generatepseudovariables();
+
   return (
     <div className={clsx(classes.BorderField, props.className)}>
       <TextField
@@ -16,11 +21,26 @@ export const BorderField = function (props: IBorderFieldProps) {
         onChange={props.onChange}
         onChangeRelation={props.onChangeRelation}
       />
+      size
+      <UnitField
+        onBreakReference={border.createOnBreakrelation(variables.size)}
+        onChange={border.createOnChange(variables.size)}
+        onChangeRelation={border.createOnChangeRelation(variables.size)}
+        variable={variables.size}
+      />
+      style
+      <TextField
+        onBreakReference={border.createOnBreakrelation(variables.style)}
+        onChange={border.createOnChange(variables.style)}
+        onChangeRelation={border.createOnChangeRelation(variables.style)}
+        variable={variables.style}
+      />
+      color
       <ColorField
-        variable={props.variable}
-        onBreakReference={props.onBreakReference}
-        onChange={props.onChange}
-        onChangeRelation={props.onChangeRelation}
+        onBreakReference={border.createOnBreakrelation(variables.color)}
+        onChange={border.createOnChange(variables.color)}
+        onChangeRelation={border.createOnChangeRelation(variables.color)}
+        variable={variables.color}
       />
     </div>
   );
---
+++ src/app/Editor/hooks/useCSSBorder.ts
@@ -0,0 +1,131 @@
+import { IVariable, VariableType } from "@store/theming/types";
+import Color from "color";
+import { TryCatch } from "tiinvo";
+import {
+  OnBreakReferenceHandler,
+  OnChangeHandler,
+  OnChangeRelationHandler,
+} from "../types/fields";
+import useVariableEditing from "./useVariableEditing";
+
+export interface IUseCSSBorderReturnValue {
+  generatepseudovariables(): Record<keyof IBorder, IVariable>;
+  createOnBreakrelation: (pseudovariable: IVariable) => OnBreakReferenceHandler;
+  createOnChange: (pseudovariable: IVariable) => OnChangeHandler;
+  createOnChangeRelation: (
+    pseudovariable: IVariable
+  ) => OnChangeRelationHandler;
+}
+
+export interface IBorder {
+  size: string;
+  style: string;
+  color: string;
+}
+
+export type PseudoVariables = Record<keyof IBorder, IVariable>;
+
+const splitToParts = (bordervalue: string): string[] => bordervalue.split(" ");
+const createborder = (chunks: string[]): IBorder => ({
+  color: chunks[2],
+  size: chunks[0],
+  style: chunks[1],
+});
+const generatepseudovariable = (
+  variable: IVariable,
+  name: string,
+  type: VariableType,
+  value: any
+): IVariable => ({
+  domain: variable.domain,
+  name: variable.name + "__" + name,
+  type,
+  element: variable.element,
+  value,
+});
+const ensurecolor = (maybecolor: string, fallback: string) =>
+  TryCatch(Color, maybecolor).mapOrElse(
+    () => fallback,
+    () => maybecolor
+  );
+const mergeborder = (
+  size: string = "",
+  style: string = "",
+  color: string = ""
+) => [size, style, color].join(" ");
+
+function updateborder(
+  pseudovariable: IVariable,
+  pseudovariables: PseudoVariables,
+  value: string
+) {
+  switch (pseudovariable.type) {
+    case "color":
+      return mergeborder(
+        pseudovariables.size.value,
+        pseudovariables.style.value,
+        value
+      );
+    case "text":
+      return mergeborder(
+        pseudovariables.size.value,
+        value,
+        pseudovariables.color.value
+      );
+    case "unit":
+      return mergeborder(
+        value,
+        pseudovariables.style.value,
+        pseudovariables.color.value
+      );
+  }
+}
+
+export default function useCSSBorder(
+  variable: IVariable,
+  fallbacksize: string = "0",
+  fallbackstyle: string = "none",
+  fallbackcolor: string = "hsl(0, 0%, 0%)"
+): IUseCSSBorderReturnValue {
+  const variableediting = useVariableEditing(variable.domain);
+  const chunks = splitToParts(variable.value ?? "0 none");
+  const parsedborder = createborder(chunks);
+  const pseudovariables = {
+    color: generatepseudovariable(
+      variable,
+      "color",
+      "color",
+      ensurecolor(parsedborder.color, fallbackcolor)
+    ),
+    size: generatepseudovariable(
+      variable,
+      "size",
+      "unit",
+      parsedborder.size ?? fallbacksize
+    ),
+    style: generatepseudovariable(
+      variable,
+      "style",
+      "text",
+      parsedborder.style ?? fallbackstyle
+    ),
+  };
+
+  return {
+    generatepseudovariables: () => pseudovariables,
+    createOnBreakrelation: (pseudovariable) => {
+      return () => {};
+    },
+    createOnChange: (pseudovariable) => {
+      return (value) => {
+        variableediting.update({
+          ...variable,
+          value: updateborder(pseudovariable, pseudovariables, value),
+        });
+      };
+    },
+    createOnChangeRelation: (pseudovariable) => {
+      return (variablename) => {};
+    },
+  };
+}
