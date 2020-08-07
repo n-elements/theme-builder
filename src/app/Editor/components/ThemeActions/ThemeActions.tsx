a17ee5167269c025ad876ae67a7ecb133ea86f26
octod <iamoctod@gmail.com>
2020-08-07 09:15:53 +0200

    chore: adds/removes querystring on preview open/close

--- src/app/Editor/components/ThemeActions/ThemeActions.tsx
+++ src/app/Editor/components/ThemeActions/ThemeActions.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import classes from "./ThemeActions.module.css";
-import clsx from "clsx";
+import useCSSExport from "@app/Editor/hooks/useCSSExport";
 import { Button } from "@components/Button";
 import { Undo } from "@components/Icons/16x";
+import { PreviewFrame } from "@components/PreviewFrame";
 import useThemeReset from "@hooks/useThemeReset";
+import clsx from "clsx";
+import React, { useState } from "react";
 import { defineMessages, useIntl } from "react-intl";
 import Modal from "react-modal";
-import useCSSExport from "@app/Editor/hooks/useCSSExport";
-import { PreviewFrame } from "@components/PreviewFrame";
+import { useHistory, useLocation } from "react-router-dom";
 import PreviewTheme from "../PreviewTheme";
-import { useLocation } from "react-router-dom";
+import classes from "./ThemeActions.module.css";
 
 const messages = defineMessages({
   downloadTheme: {
@@ -39,16 +39,19 @@ export const ThemeActions = function ({
   const intl = useIntl();
   const location = useLocation();
   const exported = useCSSExport();
+  const history = useHistory();
   const handleReset = useThemeReset();
   const [modalIsOpen, setModalIsOpen] = useState(
     location.search.includes("show-preview")
   );
 
   const openModal = () => {
+    history.push(location.pathname + "?show-preview");
     setModalIsOpen(true);
   };
 
   const closeModal = () => {
+    history.push(location.pathname);
     setModalIsOpen(false);
   };
 
